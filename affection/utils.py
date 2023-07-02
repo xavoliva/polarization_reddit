76058bb173f8ebba1ecb21be2d83695997c05668
Xavi Oliva <di93fup@cm2login1.cos.lrz.de>
2023-07-02 19:29:28 +0200

    add affective polarization utils

---
+++ affection/utils.py
@@ -0,0 +1,12 @@
+from typing import Dict
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+def get_sentiment_score(comment: str) -> Dict[str, float]:
+    # Create a SentimentIntensityAnalyzer object
+    sia = SentimentIntensityAnalyzer()
+
+    # Polarity scores are floats within the range [-1.0, 1.0]
+    polarity_scores = sia.polarity_scores(comment)
+
+    return polarity_scores
