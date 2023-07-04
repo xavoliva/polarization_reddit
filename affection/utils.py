c89dc0e54a1d6dceed1dc8a94242fcba6ea5e469
Xavi Oliva <di93fup@cm2login1.cos.lrz.de>
2023-07-04 20:17:11 +0200

    update dem/rep keywords and add logging to affective polarization

--- affection/utils.py
+++ affection/utils.py
@@ -2,11 +2,11 @@ from typing import Dict
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-def get_sentiment_score(comment: str) -> Dict[str, float]:
+def get_compound_sentiment_score(comment: str) -> Dict[str, float]:
     # Create a SentimentIntensityAnalyzer object
     sia = SentimentIntensityAnalyzer()
 
     # Polarity scores are floats within the range [-1.0, 1.0]
-    polarity_scores = sia.polarity_scores(comment)
+    polarity_scores = sia.polarity_scores(comment)["compound"]
 
     return polarity_scores
