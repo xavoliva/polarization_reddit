caa7a1a0acf8720c63c0e84293eaee366584abb7
Xavi Oliva <di93fup@cm2login1.cos.lrz.de>
2023-07-08 10:13:52 +0200

    refine and finalize abortion keyword list

--- events/abortion_constants.py
+++ events/abortion_constants.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 from events.utils import get_event_regex
@@ -9,6 +8,10 @@ from events.utils import get_event_regex
 # https://en.wikipedia.org/wiki/Abortion_debate
 ABORTION_KEYWORDS = [
     "abortion",
+    "fetus",
+    "pregnancy",
+    "unborn",
+    "birth control",
     "planned parenthood",
     "roe vs wade",
     "pro choice",
@@ -20,46 +23,61 @@ ABORTION_KEYWORDS = [
 
 # TODO: ADD RELEVANT EVENT KEYWORDS
 ABORTION_EVENTS_INFO = {
-    "dr_george_tiller_shooting": {
-        "name": "Dr. George Tiller shooting",
-        "date": pd.to_datetime("2009-05-31"),
-        "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
-    },
-    "north_dakota_heartbeat_bill": {
-        "name": "North Dakota heartbeat bill",
-        "date": pd.to_datetime("2013-03-26"),
-        "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
-    },
-    "texas_house_bill_2": {
-        "name": "Texas House Bill 2",
-        "date": pd.to_datetime("2013-07-18"),
-        "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
-    },
     "colorado_springs_shooting": {
         "name": "Colorado Springs shooting",
         "date": pd.to_datetime("2015-11-27"),
         "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
+        "regex": get_event_regex(
+            ABORTION_KEYWORDS,
+            [
+                "colorado springs",
+                "shooting",
+            ],
+            "and",
+        ),
     },
     "womans_health_vs_hellerstedt": {
         "name": "Whole Woman's Health v. Hellerstedt",
         "date": pd.to_datetime("2016-06-27"),
         "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
+        "regex": get_event_regex(
+            ABORTION_KEYWORDS,
+            [
+                "supreme court",
+                "ruling",
+                "lawsuit",
+                "health v hellerstedt",
+            ],
+            "and",
+        ),
     },
     "ohio_heartbeat_bill": {
         "name": "Ohio heartbeat bill",
         "date": pd.to_datetime("2019-11-04"),
         "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
+        "regex": get_event_regex(
+            ABORTION_KEYWORDS,
+            [
+                "ohio",
+                "heartbeat bill",
+                "six week",
+            ],
+            "and",
+        ),
     },
     "georgia_heartbeat_bill": {
         "name": "Georgia heartbeat bill",
         "date": pd.to_datetime("2019-04-04"),
         "relevant_dates": {},
-        "regex": get_event_regex(ABORTION_KEYWORDS, [], "or"),
+        "regex": get_event_regex(
+            ABORTION_KEYWORDS,
+            [
+                "georgia",
+                "heartbeat bill",
+                "six week",
+                "constitutional",
+            ],
+            "and",
+        ),
     },
 }
