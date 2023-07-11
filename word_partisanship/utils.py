47e00c462ca01d0c325866ddb0eaf577d64c0ffd
Xavi Oliva <di93fup@cm2login1.cos.lrz.de>
2023-07-11 11:34:14 +0200

    change sign of log odds ratio (positive for right-wing)

--- word_partisanship/utils.py
+++ word_partisanship/utils.py
@@ -5,17 +5,16 @@ import numpy as np
 
 def logodds_with_prior(
     term_vec,
-    dem_term_vec,
     rep_term_vec,
+    dem_term_vec,
     zscore=True,
 ):
     """
     Weighted log odds ratio with uninformative Dirichlet prior, as defined in
     https://bookdown.org/Maxine/tidy-text-mining/weighted-log-odds-ratio.html
     """
-    n_dem = np.sum(dem_term_vec)
     n_rep = np.sum(rep_term_vec)
-
+    n_dem = np.sum(dem_term_vec)
 
     alpha_0 = np.sum(term_vec)
 
@@ -23,24 +22,24 @@ def logodds_with_prior(
 
     # check validitiy of input
     assert not has_zero
-    assert alpha_0 == n_dem + n_rep
+    assert alpha_0 == n_rep + n_dem
 
     alpha_w = term_vec
-    y_w_dem = dem_term_vec
     y_w_rep = rep_term_vec
+    y_w_dem = dem_term_vec
 
-    omega_w_dem = (y_w_dem + alpha_w) / (
-        (n_dem + alpha_0) - (y_w_dem + alpha_w)
-    )  # odds in group dem
     omega_w_rep = (y_w_rep + alpha_w) / (
         (n_rep + alpha_0) - (y_w_rep + alpha_w)
     )  # odds in group rep
+    omega_w_dem = (y_w_dem + alpha_w) / (
+        (n_dem + alpha_0) - (y_w_dem + alpha_w)
+    )  # odds in group dem
 
-    delta_w_dem_rep = np.log(omega_w_dem) - np.log(omega_w_rep)  # eqn 16
+    delta_w_rep_dem = np.log(omega_w_rep) - np.log(omega_w_dem)   # eqn 16
 
     if zscore:
-        sigma2_w_dem_rep = 1 / (y_w_dem + alpha_w) + 1 / (y_w_rep + alpha_w)  # eqn 20
-        zeta_w_dem_rem = delta_w_dem_rep / np.sqrt(sigma2_w_dem_rep)   # eqn 22
-        return zeta_w_dem_rem
+        sigma2_w_rep_dem = 1 / (y_w_rep + alpha_w) + 1 / (y_w_dem + alpha_w)  # eqn 20
+        zeta_w_rep_dem = delta_w_rep_dem / np.sqrt(sigma2_w_rep_dem)   # eqn 22
+        return zeta_w_rep_dem
 
-    return delta_w_dem_rep
+    return (delta_w_rep_dem, zeta_w_rep_dem)
